520ad29039d15e83d9fee996c4c7fc865a25e84f
xuzhen <xuzhen0913@163.com>
2017-08-23 17:55:29 +0800

    823

--- EUV/BS/test0C1DataBaseDelete.py
+++ EUV/BS/test0C1DataBaseDelete.py
@@ -1,25 +1,39 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+import unittest
+from time import sleep
 import MySQLdb
 
-# 打开数据库连接
-conn = MySQLdb.connect(
-    host="172.16.40.250",
-    port=3306,
-    user="sito",
-    passwd="tianhuzuji@91.112",
-    db="yuv_test",
-    charset="utf8")
 
-cursor = conn.cursor()
+class DataBaseTest(unittest.TestCase):
+    """数据库测试"""
 
-# 清除数据库中表内容
+    def setUp(self):
+        print("test case1 start"),
 
-# 删除用户中以xuzhen开头的数据
-cursor.execute("DELETE FROM st_sa_user WHERE user_name LIKE 'test%';")
-# 删除角色中以角色开头的数据
-cursor.execute("delete from st_sa_role WHERE role_name LIKE '角色%';")
+    def tearDown(self):
+        print("test1 case end")
 
-cursor.close()
-conn.commit()
-conn.close()
-print "Done"
+    def testDataBaseDelete(self):
+        """数据库删除用户和角色"""
+        # 打开数据库连接
+        conn = MySQLdb.connect(
+            host="172.16.40.250",
+            port=3306,
+            user="sito",
+            passwd="tianhuzuji@91.112",
+            db="yuv_test",
+            charset="utf8")
+
+        cursor = conn.cursor()
+
+        # 清除数据库中表内容
+
+        # 删除用户中以xuzhen开头的数据
+        cursor.execute("DELETE FROM st_sa_user WHERE user_name LIKE 'test%';")
+        # 删除角色中以角色开头的数据
+        cursor.execute("delete from st_sa_role WHERE role_name LIKE '角色%';")
+
+        cursor.close()
+        conn.commit()
+        conn.close()
+        print "Done"
