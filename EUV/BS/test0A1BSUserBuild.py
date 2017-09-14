70981fb7e35bb37127ca72c9e0656aeaf87f020a
admin <xuzhen0913@163.com>
2017-09-14 19:21:53 +0800

    9.14

--- EUV/BS/test0A1BSUserBuild.py
+++ EUV/BS/test0A1BSUserBuild.py
@@ -2,6 +2,10 @@
 import unittest
 from selenium import webdriver
 from time import sleep
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
 class BSUserBuild(unittest.TestCase):
@@ -16,16 +20,14 @@ class BSUserBuild(unittest.TestCase):
         # self.browser = webdriver.Chrome(executable_path='/Users/xuzhen/chromedriver')
         self.browser.maximize_window()
         self.browser.get("http://172.16.40.240:8888/sitopeuv")
-        # self.browser.get("http://114.215.94.141:8060/sitopeuv")
-        # self.browser.get("http://localhost:8080/sitopeuv/")
         sleep(3)
         # 登录密码
         self.browser.find_element_by_id('userName').clear()
-        self.browser.find_element_by_id('userName').send_keys('biandongfeng')
+        self.browser.find_element_by_id('userName').send_keys('zhenzhen')
         self.browser.find_element_by_id('userPwd').clear()
-        self.browser.find_element_by_id('userPwd').send_keys('12345678')
+        self.browser.find_element_by_id('userPwd').send_keys('123456')
         # 登录
-        self.browser.find_element_by_xpath('/html/body/div/div[2]/div[2]/p[3]/input').click()
+        self.browser.find_element_by_xpath('//input[@value="登录"]').click()
         # 等待
         sleep(5)
 
@@ -41,7 +43,7 @@ class BSUserBuild(unittest.TestCase):
         self.browser.execute_script(js)
         sleep(3)
         # 系统管理
-        self.browser.find_element_by_xpath('/html/body/div/index-header/div/div[2]/div[1]/div/div[11]/div[1]/i').click()
+        self.browser.find_element_by_xpath('//div[@expander-title="系统管理"]').click()
         sleep(1)
         self.browser.find_element_by_link_text('用户管理').click()
         # 滚动翻页
@@ -51,24 +53,22 @@ class BSUserBuild(unittest.TestCase):
         sleep(1)
         self.browser.find_element_by_id('button2').click()
         # 表单
-        self.browser.find_elements_by_xpath('//*[@id="name"]')[0].send_keys(BSUserBuild.username)
-        self.browser.find_elements_by_xpath('//*[@id="name"]')[1].send_keys(u'许臻')
-        self.browser.find_elements_by_xpath('//*[@id="name"]')[2].send_keys('123456')
-        self.browser.find_elements_by_xpath('//*[@id="name"]')[3].send_keys('123456')
+        self.browser.find_element_by_xpath('//input[@placeholder="请输入用户名"]').send_keys(BSUserBuild.username)
+        self.browser.find_element_by_xpath('//input[@placeholder="请输入真实姓名"]').send_keys(u'许臻')
+        self.browser.find_element_by_xpath('//input[@placeholder="请输入密码"]').send_keys('123456')
+        self.browser.find_element_by_xpath('//input[@placeholder="请输入确认密码"]').send_keys('123456')
         self.browser.find_elements_by_id('tel')[0].send_keys('15609100803')
         self.browser.find_elements_by_id('tel')[1].send_keys('029-123456')
 
-        self.browser.find_element_by_xpath(
-            '/html/body/div/index-header/div/div[2]/div[2]/div[2]/div/form/div[7]/span/div/div/button').click()
-        self.browser.find_element_by_link_text('企业管理员').click()
+        self.browser.find_element_by_xpath('//button[@ng-class="settings.buttonClasses"]').click()
+        self.browser.find_element_by_link_text('巡检组长').click()
         self.browser.find_element_by_xpath(
             '/html/body/div/index-header/div/div[2]/div[2]/div[2]/div/form/div[7]/span/div/div/button').click()
         self.browser.find_element_by_id('new-save').click()
 
         # 断言页面上新添加的元素是否和断言一致
         sleep(3)
-        result = self.browser.find_element_by_xpath(
-            '/html/body/div/index-header/div/div[2]/div[2]/div[1]/table/tbody/tr[1]/td[1]').text
+        result = self.browser.find_elements_by_xpath('//td[@data-title-text="用户名"]')[0].text
         print(result),
         sleep(1)
         self.assertEqual(result, u'用户test', msg="添加的用户名与网页上显示的用户名不同！")
--- EUV/BS/test0A2BSRoleBuild.py
+++ EUV/BS/test0A2BSRoleBuild.py
@@ -16,16 +16,14 @@ class BSRoleBuild(unittest.TestCase):
         # self.browser = webdriver.Chrome(executable_path='/Users/xuzhen/chromedriver')
         self.browser.maximize_window()
         self.browser.get("http://172.16.40.240:8888/sitopeuv")
-        # self.browser.get("http://114.215.94.141:8060/sitopeuv")
-        # self.browser.get("http://localhost:8080/sitopeuv/")
         sleep(3)
         # 登录密码
         self.browser.find_element_by_id('userName').clear()
-        self.browser.find_element_by_id('userName').send_keys('biandongfeng')
+        self.browser.find_element_by_id('userName').send_keys('zhenzhen')
         self.browser.find_element_by_id('userPwd').clear()
-        self.browser.find_element_by_id('userPwd').send_keys('12345678')
+        self.browser.find_element_by_id('userPwd').send_keys('123456')
         # 登录
-        self.browser.find_element_by_xpath('/html/body/div/div[2]/div[2]/p[3]/input').click()
+        self.browser.find_element_by_xpath('//input[@value="登录"]').click()
         # 等待
         sleep(5)
 
@@ -41,7 +39,7 @@ class BSRoleBuild(unittest.TestCase):
         self.browser.execute_script(js)
         sleep(3)
         # 系统管理
-        self.browser.find_element_by_xpath('/html/body/div/index-header/div/div[2]/div[1]/div/div[11]/div[1]/i').click()
+        self.browser.find_element_by_xpath('//div[@expander-title="系统管理"]').click()
         sleep(1)
         self.browser.find_element_by_link_text('角色权限').click()
         # 滚动翻页
